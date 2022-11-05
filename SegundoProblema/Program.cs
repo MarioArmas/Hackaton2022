335d037164e99ac3a8bfca6e37ad0f4b46b49018
LUIS ALEJANDRO GIRON TELLO <tatotello210@gmail.com>
2022-11-05 15:24:16 -0600

    Second

---
+++ SegundoProblema/Program.cs
@@ -0,0 +1,22 @@
+﻿int incremento = 0, contador =0;
+Console.WriteLine("Ingresar Texto");
+string texto = Console.ReadLine();
+string[] info = new string[texto.Length];
+string[] dic = new string[] { "A","E","I","O","U" };
+while (incremento < texto.Length)
+{
+    texto[incremento].ToString();
+    info[incremento] = texto[incremento].ToString();
+    incremento++;
+}
+for (int j = 0; j < texto.Length; j++)
+{
+    for (int i = 0; i < dic.Length; i++)
+    {
+        if (info[j].ToUpper() == dic[i])
+        {     
+            contador++;
+        }
+    }
+}
+Console.WriteLine("Hay " + contador + " vocales :D");
