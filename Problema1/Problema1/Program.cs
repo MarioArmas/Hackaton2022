fa8c1395ddb288622cecfc7ff018d99eee860d15
Mario Armas <mario.r.armas@gmail.com>
2022-11-05 16:17:10 -0600

    Refactor second problem, Add poblem 4

---
+++ Problema1/Problema1/Program.cs
@@ -0,0 +1,89 @@
+﻿void menu() {
+    string num1 = "";
+    string num2 = "";
+    Console.WriteLine("Presione (s) si desea sumar o (r) si desea restar");
+    ConsoleKey inputOperation = Console.ReadKey().Key;
+    if (inputOperation != ConsoleKey.S && inputOperation != ConsoleKey.R) {
+        Console.WriteLine("Las teclas ingresadas no son correctas");
+        return;
+    }
+    
+    Console.WriteLine("Presione (d) si decea ingresar los números en decimal, presione (b) si desea ingresarlos en binario o (h) si desea ingresarlos en hexadecimal");
+    ConsoleKey numericSystem = Console.ReadKey().Key;
+    if (numericSystem != ConsoleKey.D && numericSystem != ConsoleKey.B && numericSystem != ConsoleKey.H) {
+        Console.WriteLine("Las teclas ingresadas no son correctas");
+        return;
+    }
+    
+    Console.WriteLine("Ingrese los números que desea operar");
+    num1 = Console.ReadLine();
+    num2 = Console.ReadLine();
+
+    if (numericSystem == ConsoleKey.B)
+    {
+        num1 = binaryToDecimal(num1);
+        num2 = binaryToDecimal(num2);
+    }
+
+    if (inputOperation == ConsoleKey.S)
+    {
+        int result = Convert.ToInt32(num1) + Convert.ToInt32(num2);
+    }
+
+    if (inputOperation == ConsoleKey.R)
+    {
+        int result = Convert.ToInt32(num1) - Convert.ToInt32(num2);
+
+    }
+}
+
+
+string binaryToDecimal(string binary) {
+    int decimalNum = 0;
+    int counter = 1;
+
+    for (int i = binary.Length - 1; i >= 0; i--) {
+        int val = (int)Char.GetNumericValue(binary[i]);
+        decimalNum += counter * val;
+        counter *= 2;
+    }
+
+    return decimalNum.ToString();
+}
+
+string decimalToBinary(int decimalNum) {
+    string binary = "";
+
+    while (decimalNum != 0) {
+        int digit = decimalNum % 2;
+        decimalNum -= digit;
+        binary += digit;
+        decimalNum /= 2;
+    }
+
+    char[] myArr = binary.ToCharArray();
+    Array.Reverse(myArr);
+    return new string(myArr);
+}
+
+string hexadecimalToDecimal(string hex) {
+    int decimalNum = 0;
+    int counter = 1;
+
+    for (int i = hex.Length - 1; i >= 0; i--)
+    {
+        int val;
+        if (hex[i] == 'A' || hex[i] == 'a') val = 10;
+        if (hex[i] == 'B' || hex[i] == 'b') val = 11;
+        if (hex[i] == 'C' || hex[i] == 'c') val = 12;
+        if (hex[i] == 'D' || hex[i] == 'd') val = 13;
+        if (hex[i] == 'E' || hex[i] == 'e') val = 14;
+        if (hex[i] == 'F' || hex[i] == 'f') val = 15;
+        else val = (int)Char.GetNumericValue(hex[i]);
+
+        decimalNum += counter * val;
+        counter *= 16;
+    }
+
+    return decimalNum.ToString();
+}
--- SegundoProblema/Program.cs
+++ SegundoProblema/Program.cs
@@ -1,22 +1,36 @@
-﻿int incremento = 0, contador =0;
+﻿using System.Globalization;
+using System.Text;
+
 Console.WriteLine("Ingresar Texto");
-string texto = Console.ReadLine();
-string[] info = new string[texto.Length];
-string[] dic = new string[] { "A","E","I","O","U" };
-while (incremento < texto.Length)
-{
-    texto[incremento].ToString();
-    info[incremento] = texto[incremento].ToString();
-    incremento++;
-}
-for (int j = 0; j < texto.Length; j++)
-{
-    for (int i = 0; i < dic.Length; i++)
-    {
-        if (info[j].ToUpper() == dic[i])
-        {     
-            contador++;
+string text = removeDiacritics(Console.ReadLine()).ToUpper();
+
+Console.WriteLine("Hay " + countVocals(text) + " vocales :D");
+
+int countVocals(string _text) {
+    int counter = 0;
+    string dic = "AEIOU";
+    
+    for (int j = 0; j < _text.Length; j++) {
+        for (int i = 0; i < dic.Length; i++) {
+            if (_text[j] == dic[i]) {
+                counter++;
+            }
         }
     }
+
+    return counter;
 }
-Console.WriteLine("Hay " + contador + " vocales :D");
+
+string removeDiacritics(string text) {
+    var normalizedString = text.Normalize(NormalizationForm.FormD);
+    var stringBuilder = new StringBuilder();
+
+    foreach (var c in normalizedString.EnumerateRunes()) {
+        var unicodeCategory = Rune.GetUnicodeCategory(c);
+        if (unicodeCategory != UnicodeCategory.NonSpacingMark) {
+            stringBuilder.Append(c);
+        }
+    }
+
+    return stringBuilder.ToString().Normalize(NormalizationForm.FormC);
+}
