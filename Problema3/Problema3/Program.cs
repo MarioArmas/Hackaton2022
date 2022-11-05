25fc595c4a147fdaf8cd7d50bac1e16252aa4827
Mario Armas <mario.r.armas@gmail.com>
2022-11-05 14:46:31 -0600

    Add error handling

--- Problema3/Problema3/Program.cs
+++ Problema3/Problema3/Program.cs
@@ -1,7 +1,11 @@
-﻿
-Console.WriteLine("Ingrese el número del cual desea sacar el factorial");
-int input = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(factorial(input));
+﻿Console.WriteLine("Ingrese el número del cual desea sacar el factorial");
+try {
+    int input = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Resultado: " + factorial(input));
+}
+catch {
+    Console.WriteLine("Debe ingresar un número entero");
+}
 
 int factorial(int num) {
     int result = 1;
