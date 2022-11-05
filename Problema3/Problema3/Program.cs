685e7f791070fe056ceaa04633b09cfb7b9493be
Mario Armas <mario.r.armas@gmail.com>
2022-11-05 14:27:41 -0600

    Add problema 3

---
+++ Problema3/Problema3/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Ingrese el número del cual desea sacar el factorial");
+int input = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(factorial(input));
+
+int factorial(int num) {
+    int result = 1;
+    for (int i = num; i > 0; i--) {
+        result *= i;
+    }
+
+    return result;
+}
